4c3a1b58baed03924f921faa2a7ae24c56e8b757
Luther Tychonievich <tychonievich@gmail.com>
2019-10-09 14:25:19 -0400

    autocommit caused by update

--- cal2html.py
+++ cal2html.py
@@ -292,7 +292,7 @@ def cal2html2(cal):
                 ans.append('</div>')
                 ans.append('</div>')
             elif day is None:
-                ans.append('<div class="day empty w{}"></div>'.format(wn))
+                ans.append('<div class="day empty {}"></div>'.format('Sun Mon Tue Wed Thu Fri Sat'.split()[wn]))
             wn+=1
     ans.append('</div>')
     external = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"><path fill="#fff" stroke="#36c" d="M1.5 4.518h5.982V10.5H1.5z"/><path fill="#36c" d="M5.765 1H11v5.39L9.427 7.937l-1.31-1.31L5.393 9.35l-2.69-2.688 2.81-2.808L4.2 2.544z"/><path fill="#fff" d="M9.995 2.004l.022 4.885L8.2 5.07 5.32 7.95 4.09 6.723l2.882-2.88-1.85-1.852z"/></svg>'
